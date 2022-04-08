946d12c4206ae1d225f2a4d1f052cf9d8ce4f9cd
nickarafyllis <nickarafyllis@gmail.com>
2022-04-08 05:10:02 +0300

    kane make kai trexe to tree-example me to bad.tree

--- Makefile
+++ Makefile
@@ -1,16 +1,25 @@
 C = gcc
 CFLAGS = -Wall -O2
 
-all: fork-given-tree
+all: fork-given-tree tree-example
 
 fork-given-tree: fork-given-tree.o proc-common.o
-        $(CC) -o fork-given-tree fork-given-tree.o proc-common.o
+	$(CC) -o fork-given-tree fork-given-tree.o proc-common.o
 
 proc-common.o: proc-common.c proc-common.h
-        $(CC) $(CFLAGS) -o proc-common.o -c proc-common.c
+	$(CC) $(CFLAGS) -o proc-common.o -c proc-common.c
 
 fork-given-tree.o: fork-given-tree.c proc-common.h
-        $(CC) $(CFLAGS) -o fork-given-tree.o -c fork-given-tree.c
+	$(CC) $(CFLAGS) -o fork-given-tree.o -c fork-given-tree.c
+
+tree-example: tree-example.o tree.o proc-common.o
+	$(CC) -o tree-example tree-example.o tree.o proc-common.o
+
+tree.o: tree.c tree.h proc-common.h
+	$(CC) $(CFLAGS) -o tree.o -c tree.c 
+
+tree-example.o: tree-example.c tree.h proc-common.h
+	$(CC) $(CFLAGS) -o tree-example.o -c tree-example.c 
 
 clean:
-        rm -f proc-common.o fork-given-tree.o
+	rm -f proc-common.o fork-given-tree.o tree.o tree-example.o
--- tree-example.c
+++ tree-example.c
@@ -8,31 +8,47 @@
 #include "tree.h"
 #include "proc-common.h"
 
+#define SLEEP_PROC_SEC  10
+#define SLEEP_TREE_SEC  3
 
-void fork_procs( struct tree_node *pr, pid_t pd){
-	change_pname(pr->name);
-	uint k=pr->nr_children;
-	if(k=0){
-		sleep(10);
+void fork_procs(struct tree_node *ptr){
+
+	change_pname(ptr->name);
+
+	uint k=ptr->nr_children;
+	if(k==0){
+		sleep(SLEEP_PROC_SEC);
 		exit(1);
 	}
 	else if(k>0){
 
+		pid_t pd; 
+	    int status;
+
 		pd=fork();
 
 		if (pd< 0) {
-                /* fork failed */
-                perror("fork");
-                exit(1);
+			/* fork failed */
+            perror("fork");
+            exit(1);
         }
-                if (pd== 0) {         
-					for(int i=0; i<k; i++){
-						fork_procs(pr->children+i,pd);
-						}
-						exit(1);
-						}
+        if (pd== 0) {         
+			for(int i=0; i<k; i++){
+				fork_procs(ptr->children+i);
+				
+			}
+			/*exit(1);*/
+		}
+
+	pd = wait(&status);
+    	explain_wait_status(pd, status);
+
 	}
-	/*else perror();*/
+	
+	
+    /*exit(1);*/
+	
+	
 }
 
 int main(int argc, char *argv[])
@@ -51,18 +67,27 @@ int main(int argc, char *argv[])
     int status;
 
 	pid = fork();
-        if (pid < 0) {
-                perror("main: fork");
-                exit(1);
-        }
-        if (pid == 0) {
-                /* Child */
-                fork_procs(root,pid);
-                exit(1);
-        }
 
+	if (pid < 0) {
+		perror("main: fork");
+        exit(1);
+    }
+    if (pid == 0) {
+        /* Child */
+        fork_procs(root);
+        exit(1);
+    }
 
+    /* for ask2-{fork, tree} */
+    sleep(SLEEP_TREE_SEC);   
+    
+	/* Print the process tree-root at pid */
 	show_pstree(pid);
+
+
+    /* Wait for the root of the process tree to terminate */
+    pid = wait(&status);
+    explain_wait_status(pid, status);
 
 	return 0;
 }
